e308109a06e34b85cd4f19e3b904bf6a199b002d
Ask-Archer <75997124+Ask-Archer@users.noreply.github.com>
2021-03-17 01:47:56 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,54 @@
+from bs4 import BeautifulSoup
+import requests
+# import lxml
+#
+# with open("website.html") as file:
+#     contents = file.read()
+#
+# soup = BeautifulSoup(contents, 'lxml')
+# print(soup.title.string)
+#
+# all_anchor_tags = soup.find_all(name="a")
+# print(all_anchor_tags)
+# for tag in all_anchor_tags:
+#     #print(tag.getText())
+#     print(tag.get("href"))
+#
+# heading = soup.find(name = "h1", id= "name")
+# print(heading)
+#
+# section_heading = soup.find(name="h3",class_ = "heading")
+# print(section_heading.get("class"))
+# company_url = soup.select_one(selector="p a")
+#
+# print(company_url)
+#
+# headings = soup.select(".heading")
+# print(headings)
+response = requests.get("https://news.ycombinator.com/")
+yc_webpage = response.text
+
+soup = BeautifulSoup(yc_webpage, "html.parser")
+articles = soup.find_all(name="a", class_= "storylink")
+
+article_texts = []
+article_links = []
+
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.get("href")
+    article_links.append(link)
+
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_= "score")]
+
+largest_number = max(article_upvotes)
+largest_index = article_upvotes.index(largest_number)
+
+print(article_texts[largest_index])
+print(article_links[largest_index])
+
+print(article_upvotes)
+print(article_links)
+print(article_texts)
+print(int(article_upvotes[0].split()[0]))
